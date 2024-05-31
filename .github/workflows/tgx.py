5e3e8a52716dbed33933376ed3a873dca44f4092
szchenxu <65478162@qq.com>
2024-05-31 17:32:57 +0800

    Update tgx.py

--- .github/workflows/tgx.py
+++ .github/workflows/tgx.py
@@ -7,13 +7,15 @@ from telethon.sync import TelegramClient
   
 api_id = os.environ.get("TG_API_ID")
 api_hash = str(os.environ.get("TG_API_HASH"))
+print(f"api_id")
+print(api_hash)
 
-session_name = 'chenxu'
-client = TelegramClient(session_name, api_id, api_hash)
-client.start()
-client.send_message("@aishegongkubot", '/sign')
-client.send_message("@DogeSGK_bot", '/sign')
-client.send_message("@yuehanbot", '/sign')
-client.send_message("@sgkvipbot", '/sign')
-client.send_message("@InfSGK_bot", '/sign')
-client.disconnect()
+#session_name = 'chenxu'
+#client = TelegramClient(session_name, api_id, api_hash)
+#client.start()
+#client.send_message("@aishegongkubot", '/sign')
+#client.send_message("@DogeSGK_bot", '/sign')
+#client.send_message("@yuehanbot", '/sign')
+#client.send_message("@sgkvipbot", '/sign')
+#client.send_message("@InfSGK_bot", '/sign')
+#client.disconnect()
