9de544e8ad9614d8d21e2bbbc2745f3f0d0ce4c6
szchenxu <65478162@qq.com>
2024-05-31 18:28:25 +0800

    Update tgx.py

--- .github/workflows/tgx.py
+++ .github/workflows/tgx.py
@@ -9,9 +9,7 @@ api_id = os.environ.get("TG_API_ID")
 api_hash = str(os.environ.get("TG_API_HASH"))
 
 #print(f"文件路径是：{os.getcwd()}")
-
-session_name = 'chenxu'
-client = TelegramClient(session_name, api_id, api_hash)
+client = TelegramClient('chenxu', api_id, api_hash)
 client.start()
 client.send_message("@aishegongkubot", '/sign')
 client.send_message("@DogeSGK_bot", '/sign')
