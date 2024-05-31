184601b6a40aa7fa51d0e027ffe4e5e28f69955c
szchenxu <65478162@qq.com>
2024-05-31 17:11:43 +0800

    Update tgx.py

--- .github/workflows/tgx.py
+++ .github/workflows/tgx.py
@@ -1,15 +1,12 @@
 import os
 import sys
 import time
-import zipfile
 from telethon.sync import TelegramClient
 # Use your own values from my.telegram.org
-wget https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1717145585689/chenxu.zip
-with zipfile.ZipFile('chenxu.zip', 'r') as zip_file:  
-    zip_file.extractall()    
+
   
-#api_id = os.environ.get("TG_API_ID")
-#api_hash = str(os.environ.get("TG_API_HASH"))
+api_id = os.environ.get("TG_API_ID")
+api_hash = str(os.environ.get("TG_API_HASH"))
 
 session_name = 'chenxu'
 client = TelegramClient(session_name, api_id, api_hash)
