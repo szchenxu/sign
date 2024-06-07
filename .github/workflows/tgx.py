01923ad96a287d979270ea084942f11ef2f84469
szchenxu <65478162@qq.com>
2024-06-07 16:47:03 +0800

    Update tgx.py

--- .github/workflows/tgx.py
+++ .github/workflows/tgx.py
@@ -3,10 +3,9 @@ import sys
 import time
 from telethon.sync import TelegramClient
 # Use your own values from my.telegram.org
-  
+sys.path.append('..')
 api_id = os.environ.get("TG_API_ID")
 api_hash = str(os.environ.get("TG_API_HASH"))
-sys.path.append(r'.github/workflows'）
 #print(f"文件路径是：{os.getcwd()}")
 client = TelegramClient('chenxu', api_id, api_hash)
 client.start()
