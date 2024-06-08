e176af68c09d777560ab33a6fa5cda795c2939b0
szchenxu <65478162@qq.com>
2024-06-08 15:38:01 +0800

    Add files via upload

---
+++ wechatpush.py
@@ -0,0 +1,15 @@
+import requests
+
+def send_wechat(msg):
+    token = os.environ.get("WECHATPUSH_TOKEN")
+    title = 'github'
+    content = msg
+    template = 'html'
+    url = f"https://www.pushplus.plus/send?token={token}&title={title}&content={content}&template={template}"
+    print(url)
+    r = requests.get(url=url)
+    print(r.text)
+
+if __name__ == '__main__':
+    msg = '脚本已正常执行'
+    send_wechat(msg)
