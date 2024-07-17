721d913a0632b29531445e33bebfaad8a98069e1
szchenxu <65478162@qq.com>
2024-07-17 11:57:56 +0800

    Update wechatpush.py

--- wechatpush.py
+++ wechatpush.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 #def send_wechat(msg):
-    token = os.environ.get("WECHATPUSH_TOKEN")
+    token = str(os.environ.get("WECHATPUSH_TOKEN"))
     title = 'github'
     content = '脚本已正常执行'
     template = 'html'
