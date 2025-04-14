972aff38002470c9c4095d92db1655e096df715d
szchenxu <65478162@qq.com>
2025-04-14 09:00:15 +0800

    Update sign.py

--- sign.py
+++ sign.py
@@ -42,12 +42,9 @@ def send_messages(api_id, api_hash):
     client.start()
 
     # 发送消息
-    client.send_message("@aishegongkubot", '/sign')
     client.send_message("@yuehanbot", '/sign')     
     client.send_message("@sgkvipbot", '/sign')    
     client.send_message("@DingDangCats_Bot", '/qd')
-    client.send_message("@pingansgk_bot", '/qd')   
-    client.send_message("@qingfeng888bot", '/qd')  
     client.send_message("@Zonesgk_bot", '/qd')  
     # 断开客户端连接
     client.disconnect()
